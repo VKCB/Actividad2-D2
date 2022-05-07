d7bb0101f31763c316242de744cb71389c715c05
Rayo <drayo.dard.16@gmail.com>
2022-05-07 16:15:30 -0600

    se realizo el paso 6 y 7

--- web-api/src/index.js
+++ web-api/src/index.js
@@ -1,9 +1,10 @@
-iimport express from "express";
+import express from "express";
 const app = express();
 const port = process.env.PORT || 9000;
 app.get('/', (req, res) => {
-res.send('Hello World!');
+    res.send('Hello World!');
 });
 app.listen(port, () => {
 console.log(`Example app listening at http://localhost:${port}`);
 });
+
